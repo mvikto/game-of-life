2bed5781724f1fc372c42f7781defebe19590bb2
mvikto <mviktolive@gmail.com>
2022-02-10 14:58:23 +0200

    stuff

--- src/icons.tsx
+++ src/icons.tsx
@@ -37,24 +37,6 @@ export const PauseIcon: Component<ComponentProps<"svg">> = (props) => (
   </svg>
 );
 
-export const ChevronDown: Component<ComponentProps<"svg">> = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    class="feather feather-chevron-down"
-    {...props}
-  >
-    <polyline points="6 9 12 15 18 9"></polyline>
-  </svg>
-);
-
 export const XIcon: Component<ComponentProps<"svg">> = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
